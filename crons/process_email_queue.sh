3fd171504b1d53a630cdd6c6e6ad4ae5c578de4e
Ali Shaheen <ashaheen@coeus-solutions.de>
2025-03-07 14:55:30 +0100

    fix(cron): Remove cronlock from process_email_queue.sh script to avoid Redis dependency on Render

--- crons/process_email_queue.sh
+++ crons/process_email_queue.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
 cd /app
-cronlock python -m src.scripts.process_email_queues 
+# Run without cronlock since Redis isn't available
+python -m src.scripts.process_email_queues 
