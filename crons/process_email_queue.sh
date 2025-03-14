47c758812d7a6822c8895eb9e2fcd1e0f1563177
Naveed Masood <naveed.butt@workhub.ai>
2025-03-14 12:14:13 +0500

    Enhance cron scripts with start and finish logging
    - Add logging to process_email_queue.sh, send_reminders.sh, and update_call_stats.sh to indicate when each process starts and finishes.
    - This improvement aids in monitoring and debugging of scheduled tasks.

--- crons/process_email_queue.sh
+++ crons/process_email_queue.sh
@@ -2,4 +2,6 @@
 set -o errexit -o nounset -o pipefail
 cd /app
 
-cronlock python -m src.scripts.process_email_queues 
+echo "Starting Processing Email Queue at $(date)"
+cronlock python -m src.scripts.process_email_queues
+echo "Finished Processing Email Queue at $(date)"
